46f5426926c79e48094c6a649757575471249ed2
Chris Rosales <10rosachri@gmail.com>
2024-04-20 07:48:13 -0400

    Making my life easier lol

---
+++ README.md
@@ -0,0 +1,5 @@
+Everything should be self explanatory if you wanna use my .dotfiles and script
+
+BUT
+
+the only thing you need to change is line 56 of the installEverything.sh script to your username instead of 'chrig'.. unless it is chrig :skull:
--- installFont.sh
+++ installFont.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-sudo cp Liga-Sauce-Code-Pro-Medium-Nerd-Font-Complete.otf ~/usr/share/fonts
+#Moving best font to fonts directory
+sudo cp Liga-Sauce-Code-Pro-Medium-Nerd-Font-Complete.otf /usr/share/fonts
+
+#Commands to set font
+gsettings set org.gnome.desktop.interface font-name 'LigaSauceCodePro Nerd Font Medium 11'
+gsettings set org.gnome.desktop.interface document-font-name 'LigaSauceCodePro Nerd Font Medium 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'LigaSauceCodePro Nerd Font Medium 11'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'LigaSauceCodePro Nerd Font Medium 11'
+
+
+
