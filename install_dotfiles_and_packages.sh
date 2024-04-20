0d4a9acdf3ceb2d00664405097d7ea7142302ea6
Chris Rosales <10rosachri@gmail.com>
2024-04-19 20:09:53 -0400

    Config Changes

--- install_dotfiles_and_packages.sh
+++ install_dotfiles_and_packages.sh
@@ -3,8 +3,8 @@
 DOTFILES_DIR="$HOME/.dotfiles"
 PACKAGE_LIST="$DOTFILES_DIR/installed_packages.list"
 
+sudo stow -t ~/.config .config && stow -t ~ Xmodmap
+
 # Set package selections and upgrade
 sudo dpkg --set-selections < "$PACKAGE_LIST"
 sudo apt-get dselect-upgrade
-
-sudo stow -t ~/.config .config && stow -t ~ Xmodmap
