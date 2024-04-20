59255b8f943b60634ea520ca2820964b94089d67
Chris Rosales <10rosachri@gmail.com>
2024-04-19 20:12:12 -0400

    Update installed packages and dotfiles list

--- install_dotfiles_and_packages.sh
+++ install_dotfiles_and_packages.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-DOTFILES_DIR="$HOME/.dotfiles"
-PACKAGE_LIST="$DOTFILES_DIR/installed_packages.list"
-
 sudo stow -t ~/.config .config && stow -t ~ Xmodmap
 
 # Set package selections and upgrade
-sudo dpkg --set-selections < "$PACKAGE_LIST"
+sudo dpkg --set-selections < "installed_packages.list"
 sudo apt-get dselect-upgrade
--- update_dotfiles_and_packages.sh
+++ update_dotfiles_and_packages.sh
@@ -8,7 +8,6 @@ PACKAGE_LIST="$DOTFILES_DIR/installed_packages.list"
 dpkg --get-selections > "$PACKAGE_LIST"
 
 # Add, commit, and push changes
-cd "$DOTFILES_DIR"
 git add "."
 git commit -m "Update installed packages and dotfiles list"
 git push
