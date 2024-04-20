a0a7ae95015910c6ba11a4472e202ead07fb1e60
Chris Rosales <10rosachri@gmail.com>
2024-04-19 23:11:28 -0400

    changes

--- install_dotfiles_and_packages.sh
+++ install_dotfiles_and_packages.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
+# Update the package database
+sudo apt-get update
+
+# Installing stow
+sudo apt-get install stow
+
+# Installing pip
+sudo apt-get install python-pip
+
+# Installing nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# First, stow your configurations
 sudo stow -t ~/.config .config && stow -t ~ Xmodmap
 
-# Set package selections and upgrade
-sudo dpkg --set-selections < "installed_packages.list"
-sudo apt-get dselect-upgrade
+
+# Load nvm into the current shell session
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+
+# Read each line in installed_packages.list
+while IFS= read -r package || [[ -n "$package" ]]; do
+    package_name=$(echo "$package" | cut -f1)
+    echo "Attempting to install $package_name using apt-get..."
+    sudo apt-get install -y "$package_name" || {
+        echo "apt-get failed, trying with snap..."
+        sudo snap install "$package_name" || {
+            echo "snap failed, trying with pip..."
+            pip install "$package_name" || {
+                echo "pip failed, attempting to install as a Node.js package with npm..."
+                npm install -g "$package_name" || echo "$package_name installation failed by all methods."
+            }
+        }
+    }
+done < "installed_packages.list"
